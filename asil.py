a554ee86ebba3137e0e57383b1d0d8d4dde3ea89
Ogungbemi Gbenga <adaradenu01@gmail.com>
2023-07-17 06:01:09 -0700

    web deployment

--- asil.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-
-# Assert that the difference between the before_count and after_count is 1
-assert (after_count - before_count) == 1, "Failed to add new state to the `states` table"
